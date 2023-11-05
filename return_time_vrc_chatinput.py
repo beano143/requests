feb8e7201a22904aff4010c3afe78ffe5c9be7b8
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-05 17:16:37 -0600

    Create return_time_vrc_chatinput.py

---
+++ return_time_vrc_chatinput.py
@@ -0,0 +1,18 @@
+import time 
+import os
+
+os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+time.tzset()
+val  = time.strftime('%X')
+val = val.split(":")
+
+if int(val[0]) >= 12:
+  val[0] = int(val[0]) - 12
+  val[0] = str(val[0])
+  
+val1 = val[1].split("0")
+
+if val1[0] == "0":
+  val1 = val1[1]
+  
+print(f"{val[0]}:{val1})
