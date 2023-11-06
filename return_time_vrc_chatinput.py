426db400b403909a73a23535df1e478f2a5c2e36
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-05 22:06:51 -0600

    Update return_time_vrc_chatinput.py

--- return_time_vrc_chatinput.py
+++ return_time_vrc_chatinput.py
@@ -4,18 +4,28 @@ import os
 
 keybpard = conytrolletr()
 
-os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
-time.tzset()
-val  = time.strftime('%X')
-val = val.split(":")
-
-if int(val[0]) >= 12:
-  val[0] = int(val[0]) - 12
-  val[0] = str(val[0])
+def get_time_local():
+  os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+  time.tzset()
+  val  = time.strftime('%X')
+  val = val.split(":")
   
-val1 = val[1].split("0")
-
-if val1[0] == "0":
-  val1 = val1[1]
+  if int(val[0]) >= 12:
+    val[0] = int(val[0]) - 12
+    val[0] = str(val[0])
+    
+  val1 = val[1].split("0")
   
-print(f"{val[0]}:{val1})
+  if val1[0] == "0":
+    val1 = val1[1]
+    
+  return f"{val[0]}:{val1}"
+
+def return_time(time):
+  keyboard.press('y')
+  keyboard.release('y')
+
+  for char in time:
+    keyboard.press('char')
+    keyboard.release('char')
+    
