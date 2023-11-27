b4965768839d15866f113c74d3904c47a6ad38c3
Hemlock Valor <143224038+beano143@users.noreply.github.com>
2023-11-27 15:42:00 -0600

    Update return_time_vrc_chatinput.py
    fixed small errors; added comments

--- return_time_vrc_chatinput.py
+++ return_time_vrc_chatinput.py
@@ -4,8 +4,8 @@ import os
 
 keybpard = conytrolletr()
 
-def get_time_local():
-  os.environ['TZ'] = 'EST+05EDT,M4.1.0,M10.5.0'
+def get_time_local(time_zone):
+  os.environ['TZ'] = time_zone
   time.tzset()
   val  = time.strftime('%X')
   val = val.split(":")
@@ -19,18 +19,18 @@ def get_time_local():
   if val1[0] == "0":
     val1 = val1[1]
     
-  return f"{val[0]}:{val1}"
+  return f"{val[0]}:{val1}" # retuns time in standard form
 
 def return_time(time):
   keyboard.press('y')
   keyboard.release('y')
 
-  kyboard.type(f"{time})
+  kyboard.type(f"{time}")
 
   keyboard.press(enter)
-  keyboadr.release(enter)
+  keyboadr.release(enter) # simulates all the key presses to input time 
 
 while True:
-  get_time_local()
-  return_time()
-  time.sleep(10)
+  time_local = get_time_local(EST+05EDT,M4.1.0,M10.5.0')
+  return_time(time_local)
+  time.sleep(30)
